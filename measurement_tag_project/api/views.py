6542da12b9e48b938ba3fdebf2ba05f59e028b0b
Ossamoon <osamu950623@gmail.com>
2020-11-11 17:58:41 +0900

    add response.set_cookie

--- measurement_tag_project/api/views.py
+++ measurement_tag_project/api/views.py
@@ -7,4 +7,5 @@ def apifunc(request):
     red = Image.new('RGBA', (100, 100), (255,0,0,255))
     response = HttpResponse(content_type="image/png")
     red.save(response, "PNG")
+    response.set_cookie(key="ossamoon_cookie_test", value='002')
     return response
